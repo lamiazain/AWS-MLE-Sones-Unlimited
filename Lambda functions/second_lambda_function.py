cbec6eddca388d5ac1aa1ea9773058155a9ce1a5
lamiazain <36462678+lamiazain@users.noreply.github.com>
2024-08-16 21:13:39 +0300

    Update second_lambda_function.py

--- Lambda functions/second_lambda_function.py
+++ Lambda functions/second_lambda_function.py
@@ -1,25 +1,15 @@
 import json
 import boto3
 import base64
-#from sagemaker.serializers import IdentitySerializer
-#import sagemaker.session as session
 
 # Fill this in with the name of your deployed model
 ENDPOINT = 'image-classification-2022-11-25-00-24-07-438'  ## TODO: fill in
-#session= session.Session()
 runtime = boto3.Session().client('sagemaker-runtime')
-
 
 def lambda_handler(event, context):
 
     # Decode the image data
     image = base64.b64decode(event['body']['image_data'])
-    
-    # Instantiate a Predictor
-   #predictor = sagemaker.predictor.Predictor(endpoint_name=ENDPOINT, sagemaker_session=session) ## TODO: fill in
-
-    # For this model the IdentitySerializer needs to be "image/png"
-    #predictor.serializer = IdentitySerializer("image/png")
 
     # Make a prediction:
     #inferences  = predictor.predict(image)## TODO: Process the payload with your predictor## TODO: fill in
